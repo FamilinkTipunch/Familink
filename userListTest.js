0eb3312e65f3841e4b9f615d874c83f50cbd865d
Thibaut Terrasse <thibautterrasse@hotmail.fr>
2017-08-16 15:46:05 +0200

    ajout des fichiers de test pour les différentes stories (epics)

---
+++ userListTest.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import {
+  AppRegistry,
+  StyleSheet,
+  Text,
+  View
+} from 'react-native';
+
+export default class userListTest extends Component {
+
+}
+
+AppRegistry.registerComponent('FamilinkTipunch', () => FamilinkTipunch);
