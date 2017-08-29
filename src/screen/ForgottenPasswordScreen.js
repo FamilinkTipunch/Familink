84261c6aab36761dc5ac543bfcc37ed7d02195ac
Thibaut Terrasse <thibautterrasse@hotmail.fr>
2017-08-29 15:40:33 +0200

    modification ForgotPassword

--- src/screen/ForgottenPasswordScreen.js
+++ src/screen/ForgottenPasswordScreen.js
@@ -35,8 +35,6 @@ export default class ForgottenPasswordScreen extends Component {
       if (status === 1) {
         Toast.show('Un SMS vous a été envoyé');
         this.navigateToLogin();
-      } else if (status !== null) {
-        Toast.show(`Une erreur est survenue lors de la saisi du numéro avec le code erreur: ${status}`);
       }
     }
     validator = () => {
